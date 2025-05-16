14bf9f150dad6dee15f7808e686917c7862a0500
Omar Abukhalaf <134803309+OmarAbukhalaf@users.noreply.github.com>
2025-05-16 15:50:11 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -40,11 +40,7 @@ def load_models():
 def preprocess_image(image_file):
     transform = transforms.Compose([
         transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-        transforms.Normalize(
-            mean=[0.485, 0.456, 0.406], 
-            std=[0.229, 0.224, 0.225]
-        ),
+        transforms.ToTensor()
     ])
     image = Image.open(image_file).convert("RGB")
     return transform(image).unsqueeze(0)  # Shape: (1, 3, 224, 224)
